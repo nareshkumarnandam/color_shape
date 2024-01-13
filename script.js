a0c03c6bddec66a1b4ee762609952362db47409a
naresh <nareshcena666@gmail.com>
2024-01-13 21:05:37 +0530

    shape and color changer

---
+++ script.js
@@ -0,0 +1,33 @@
+var arr = ["square", "triangle", "rectangle", "circle", "diamond", "star"];
+var color = [ "red",  "green",  "cyan",  "black",  "violet",  "blue",  "yellow",  "purple",  "orange",  "lightgreen",  "brown" , "orangered"];
+
+let outer = document.getElementById("outer");
+let inner = document.getElementById("inner");
+let colorbtn = document.getElementById("color");
+let shapebtn = document.getElementById("shape");
+
+var index = 0;
+
+colorbtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  if (index === color.length) {
+    index = 0;
+  }
+  outer.style.backgroundColor = color[index];
+  index++;
+}
+
+shapebtn.onclick = function () {
+  let style = inner.classList[0];
+  let flag = true;
+  let random;
+  while (flag) {
+    random = Math.floor(Math.random() * arr.length);
+    if (style != arr[random]) {
+      flag = false;
+    }
+  }
+  inner.classList.remove(style);
+  inner.classList.add(arr[random]);
+};
